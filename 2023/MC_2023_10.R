9c0215bc743999bd0d8256361c98d7a21af13c4b
Karthik Thrikkadeeri <rikudoukarthik@gmail.com>
2023-11-13 12:17:44 +0530

    drafted latest monthly challenge

---
+++ 2023/MC_2023_10.R
@@ -0,0 +1,36 @@
+###### monthly challenge winners/results ###
+
+# basic eligible list filter
+data0 <- data_mc %>% 
+  filter(ALL.SPECIES.REPORTED == 1, DURATION.MINUTES >= 14) %>% 
+  group_by(SAMPLING.EVENT.IDENTIFIER) %>% 
+  filter(!any(OBSERVATION.COUNT == "X")) %>% 
+  ungroup()
+
+# at least 40 lists
+data1 <- data0 %>% 
+  group_by(OBSERVER.ID) %>% 
+  summarise(NO.LISTS = n_distinct(SAMPLING.EVENT.IDENTIFIER)) %>% 
+  filter(NO.LISTS >= 40)
+
+# at least 5 lists on Oct 14
+data2 <- data0 %>% 
+  group_by(OBSERVER.ID) %>% 
+  filter(DAY.M == 14) %>% 
+  summarise(NO.LISTS = n_distinct(SAMPLING.EVENT.IDENTIFIER)) %>% 
+  filter(NO.LISTS >= 5)
+
+
+results <- data1 %>% 
+  inner_join(data2, by = "OBSERVER.ID") %>% 
+  left_join(eBird_users, by = "OBSERVER.ID") %>% 
+  anti_join(filtGA, by = "OBSERVER.ID")
+
+
+# random selection 
+a <- results %>% 
+  filter(FULL.NAME != "MetalClicks Ajay Ashok") # removes NAs too
+set.seed(1)
+winner <- a %>% slice_sample(n = 1) %>% select(FULL.NAME)
+
+winner_mc_announcement <- glue("Monthly challenge winner is {winner}")
