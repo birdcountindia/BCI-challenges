2788fb1cf34a0e06947059aabd5a36ed95a86730
birdcountindia <skimmer@birdcount.in>
2024-02-27 11:48:55 +0530

    ran latest monthly challenge

--- 2024/MC_2024_01.R
+++ 2024/MC_2024_01.R
@@ -48,7 +48,7 @@ results <- data1 %>%
 # random selection 
 a <- results %>% 
   filter(FULL.NAME != "MetalClicks Ajay Ashok") # removes NAs too
-set.seed(1)
+set.seed(9)
 winner <- a %>% slice_sample(n = 1) %>% select(FULL.NAME)
 
 winner_mc_announcement <- glue("Monthly challenge winner is {winner}")
