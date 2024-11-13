aa13ae15297f5ae602fb68835e96e5b57c39655a
ron1th <ronithurs@ncf-india.org>
2024-11-14 01:23:27 +0530

    Added test code for October monthly challenge

---
+++ 2024/MC_2024_10.R
@@ -0,0 +1,31 @@
+###### monthly challenge winners/results ###
+
+# basic eligible list filter
+data0 <- data_mc %>% 
+  filter(ALL.SPECIES.REPORTED == 1, DURATION.MINUTES >= 14) %>% 
+  group_by(SAMPLING.EVENT.IDENTIFIER) %>% 
+  filter(!any(OBSERVATION.COUNT == "X")) %>% 
+  ungroup()
+
+# At least 30 lists in October
+data1 <- data0 %>% 
+  group_by(OBSERVER.ID) %>% 
+  summarise(NO.LISTS = n_distinct(SAMPLING.EVENT.IDENTIFIER)) %>% 
+  filter(NO.LISTS >= 30)
+
+# At least 4 lists on October 12
+data2 <- data0 %>% 
+  filter(DAY.M == 12) %>% 
+  group_by(OBSERVER.ID) %>% 
+  summarise(NO.LISTS.WBD = n_distinct(SAMPLING.EVENT.IDENTIFIER)) %>% 
+  filter(NO.LISTS.WBD >= 4)
+
+results <- data1 %>% 
+  inner_join(data2, by = "OBSERVER.ID") %>% 
+  left_join(eBird_users, by = "OBSERVER.ID") %>% 
+  anti_join(filtGA, by = "OBSERVER.ID")
+
+# random selection
+a <- results %>% filter(!is.na(FULL.NAME))
+winner <- sel_random_winner(results, seed = 123)
+winner_mc_announcement <- glue("Monthly challenge winner is {winner}")
