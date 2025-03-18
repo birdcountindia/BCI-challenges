c72bbd26c5853fbcc73ee3188d7a363c93651c43
ron1th <ronithurs@ncf-india.org>
2025-03-18 13:45:34 +0530

    Monyhly challenge for Feb 2025

---
+++ 2025/MC_2025_02.R
@@ -0,0 +1,31 @@
+###### monthly challenge winners/results for December 2024 ###
+
+# basic eligible list filter
+data0 <- data_mc %>% 
+  filter(ALL.SPECIES.REPORTED == 1, DURATION.MINUTES >= 14) %>% 
+  group_by(SAMPLING.EVENT.IDENTIFIER) %>% 
+  filter(!any(OBSERVATION.COUNT == "X")) %>% 
+  ungroup()
+
+# at least 40 lists
+data1 <- data0 %>% 
+  group_by(OBSERVER.ID) %>% 
+  summarise(NO.LISTS = n_distinct(SAMPLING.EVENT.IDENTIFIER)) %>% 
+  filter(NO.LISTS >= 40)
+
+# 10 from GBBC
+data2 <- data0 %>% 
+  filter(DAY.M %in% 14:17) %>% 
+  group_by(OBSERVER.ID) %>% 
+  summarise(NO.LISTS.GBBC = n_distinct(SAMPLING.EVENT.IDENTIFIER)) %>% 
+  filter(NO.LISTS.GBBC >= 10)
+
+
+results <- data1 %>% 
+  inner_join(data2, by = "OBSERVER.ID") %>% 
+  left_join(eBird_users, by = "OBSERVER.ID") %>% 
+  anti_join(filtGA, by = "OBSERVER.ID")
+
+# random selection
+winner <- sel_random_winner(results, seed = 123)
+winner_mc_announcement <- glue("Monthly challenge winner is {winner}")
