562623f371b727e85014bc82d43f9a2e668751e7
ron1th <ronithurs@ncf-india.org>
2025-06-16 14:32:54 +0530

    Added May 2025 Monthly challenge

---
+++ 2025/MC_2025_05.R
@@ -0,0 +1,24 @@
+###### monthly challenge winners/results for May 2025 ###
+
+# basic eligible list filter
+data0 <- data_mc %>% 
+  filter(ALL.SPECIES.REPORTED == 1, DURATION.MINUTES >= 14) %>% 
+  group_by(SAMPLING.EVENT.IDENTIFIER) %>% 
+  filter(!any(OBSERVATION.COUNT == "X")) %>% 
+  ungroup()
+
+# at least 20 lists from same location
+data1 <- data0 %>% 
+  group_by(OBSERVER.ID, LOCALITY.ID) %>% 
+  reframe(NO.LISTS = n_distinct(SAMPLING.EVENT.IDENTIFIER)) %>% 
+  filter(NO.LISTS >= 20)
+# observers with more than one location get advantage in random selection
+
+
+results <- data1 %>% 
+  left_join(eBird_users, by = "OBSERVER.ID") %>% 
+  anti_join(filtGA, by = "OBSERVER.ID")
+
+# random selection
+winner <- sel_random_winner(results, seed = 123)
+winner_mc_announcement <- glue("Monthly challenge winner is {winner}")
