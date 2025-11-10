b5f70e57e62587b8f492e25f8be392ca11bae234
skimmer@birdcount.in <your_email@example.com>
2025-11-10 18:08:23 +0530

    MC script for October is added

---
+++ 2025/MC_2025_10.R
@@ -0,0 +1,33 @@
+###### monthly challenge winners/results ###
+
+#Upload at least 30 eligible complete checklists in October
+#At least 5 or more checklists are uploaded on 11 October
+
+# basic eligible list filter
+data0 <- data_mc %>% 
+  filter(ALL.SPECIES.REPORTED == 1, DURATION.MINUTES >= 14) %>% 
+  group_by(SAMPLING.EVENT.IDENTIFIER) %>% 
+  filter(!any(OBSERVATION.COUNT == "X")) %>% 
+  ungroup()
+
+# At least 30 lists in October
+data1 <- data0 %>% 
+  group_by(OBSERVER.ID) %>% 
+  summarise(NO.LISTS = n_distinct(SAMPLING.EVENT.IDENTIFIER)) %>% 
+  filter(NO.LISTS >= 30)
+
+# At least 5 lists on October 11
+data2 <- data0 %>% 
+  filter(DAY.M == 11) %>% 
+  group_by(OBSERVER.ID) %>% 
+  summarise(NO.LISTS.WBD = n_distinct(SAMPLING.EVENT.IDENTIFIER)) %>% 
+  filter(NO.LISTS.WBD >= 5)
+
+results <- data1 %>% 
+  inner_join(data2, by = "OBSERVER.ID") %>% 
+  left_join(eBird_users, by = "OBSERVER.ID") %>% 
+  anti_join(filtGA, by = "OBSERVER.ID")
+
+# random selection
+winner <- sel_random_winner(results, seed = 123)
+winner_mc_announcement <- glue("Monthly challenge winner is {winner}")
